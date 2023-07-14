c555751bc550186af1518a2a22b6e1cf60df95c0
nitinrajputind <nitinrajput971625@gmail.com>
2023-07-14 19:31:29 +0530

    Initialize project using Create React App

---
+++ src/Components/FormClass.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class FormClass extends Component {
+  render() {
+    return (
+      <div>
+        <label htmlFor="name">Name: </label>
+        <input type="text" name="" id="" />
+      </div>
+    )
+  }
+}
+
+export default FormClass
